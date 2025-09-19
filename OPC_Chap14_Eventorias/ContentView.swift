de8024a794489218fdf1db671de90f52646b0e05
Hugues <hugues.bousselet@gmail.com>
2025-09-19 14:52:16 +0200

    Initial Commit

---
+++ OPC_Chap14_Eventorias/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OPC_Chap14_Eventorias
+//
+//  Created by Hugues BOUSSELET on 19/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OPC_Chap14_Eventorias/OPC_Chap14_EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  OPC_Chap14_EventoriasApp.swift
+//  OPC_Chap14_Eventorias
+//
+//  Created by Hugues BOUSSELET on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct OPC_Chap14_EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
