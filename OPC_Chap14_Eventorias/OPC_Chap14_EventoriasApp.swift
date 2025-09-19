c7e7bfc6875e519c7a57b786b274ea55f745b337
Hugues <hugues.bousselet@gmail.com>
2025-09-19 15:16:18 +0200

    Setup of Firebase for the project.

--- OPC_Chap14_Eventorias/OPC_Chap14_EventoriasApp.swift
+++ OPC_Chap14_Eventorias/OPC_Chap14_EventoriasApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct OPC_Chap14_EventoriasApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
